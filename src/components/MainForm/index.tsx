4e333b35fc99bbd61a23b532078e910205089103
rfreitas16 <rfreitas16@hotmail.com>
2025-08-22 14:36:59 -0300

    aplicando logica do formulario

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,28 +2,63 @@ import { PlayCircleIcon } from 'lucide-react';
 import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
-import { useState } from 'react';
+import { useRef} from 'react';
+import type { TaskModel } from '../../models/taskModel';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 
 export function MainForm() {
-  const [taskName, setTaskName] = useState('');
+  const {setState} = useTaskContext();
+  const taskNameInput = useRef<HTMLInputElement>(null);
+
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    console.log('deu certo');
+
+   if(taskNameInput.current === null) return;
+
+   const taskName = taskNameInput.current.value.trim();
+    if(!taskName) {
+      alert('digite o nome da tarefa')
+      return
+    }
+    //criar nova tarefa
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: 1,
+      type: 'workTime'
+    };
+
+    const secondsRemaining = newTask.duration * 60
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        config:{...prevState.config},
+        activeTask: newTask,
+        currentCycle: 1,
+        secondsRemaining,
+        formattedSecondsRemaining: '00:00',
+        tasks: [
+          ...prevState.tasks, newTask
+        ],
+      }
+    })
+   console.log(taskName);
   }
 
   return (
     <form onSubmit={handleCreateNewTask} className='form' action=''>
-      <h1>{taskName}</h1>
+      <h1>o formulario foi enviado </h1>
       <div className='formRow'>
         <DefaultInput
           labelText='task'
           id='meuid'
           type='text'
           placeholder='Digite algo'
-          value={taskName}
-          onChange={e => {
-            setTaskName(e.target.value);
-          }}
+          ref={taskNameInput}
         />
       </div>
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import { Container } from '../../components/Container';
 import { CountDown } from '../../components/CountDown';
+
 import { MainForm } from '../../components/MainForm';
 import type { TaskStateModel } from '../../models/TaskStateModel';
 import { MainTemplate } from '../../templates/MainTemplate';
@@ -10,8 +11,6 @@ export type HomeProps = {
 };
 
 export function Home(props: HomeProps) {
-  console.log(props);
-
   return (
     <MainTemplate>
       <Container>
