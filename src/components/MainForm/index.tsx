10180598456a79a2a79cf34a7b61dbb4d009fcd0
rfreitas16 <rfreitas16@hotmail.com>
2025-08-23 15:29:35 -0300

    criando array para exibir bolinhas dos ciclos

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -68,9 +68,11 @@ export function MainForm() {
       <div className='formRow'>
         <p>Lorem ipsum dolor sit amet.</p>
       </div>
+      {state.currentCycle > 0 && (
       <div className='formRow'>
         <Cycles />
       </div>
+      )}
       <div className='formRow'>
         <DefaultButton icon={<PlayCircleIcon />} />
       </div>
