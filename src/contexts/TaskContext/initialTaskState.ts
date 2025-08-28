371f2e19d1e033a557bba06c5f66d8a9606cc281
rfreitas16 <rfreitas16@hotmail.com>
2025-08-28 14:16:30 -0300

    colocando audio ao terminar atividade

--- src/contexts/TaskContext/initialTaskState.ts
+++ src/contexts/TaskContext/initialTaskState.ts
@@ -7,8 +7,8 @@ export const initialTaskState: TaskStateModel = {
   activeTask: null,
   currentCycle: 0,
   config: {
-    workTime: 25,
-    shortBreakTime: 5,
+    workTime: 1,
+    shortBreakTime: 1,
     longBreakTime: 15,
   },
 };
